841f4f1f7536617e651e8ef1f6cc67249272378b
jamissi <janyyrm@gmail.com>
2022-08-07 20:13:23 -0300

    Coloquei uma virgula

--- Desafio014.py
+++ Desafio014.py
@@ -2,4 +2,4 @@
 
 c = float(input('Digite a temperatura em °C: '))
 f = ((9 * c) / 5) + 32
-print(f'A temperatura de {c}°C corresponde a {f}°F')
+print(f'A temperatura de {c}°C, corresponde a {f}°F')
