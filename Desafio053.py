6e55b0e89a46d5f6181872b95c02cfc21a489438
jamissi <janyyrm@gmail.com>
2022-08-08 15:11:03 -0300

    Update Desafio053.py

--- Desafio053.py
+++ Desafio053.py
@@ -1,2 +1,13 @@
 #Crie um programa que leia uma frase qualquer e diga
 #Se ela é um palindromo, desconsiderando os espaços.
+frase = str(input('Digite uma frase: ')).upper().strip()
+palavras = frase.split()
+junto =''.join(palavras)
+inverso = ''
+for c in range (len(junto) -1, -1, -1):
+    inverso += junto[c]
+print(f'O inverso de {frase} é {inverso}.')
+if inverso == frase:
+    print('A frase é um palíndromo.')
+else:
+    print('A frase não é um palíndromo.')
